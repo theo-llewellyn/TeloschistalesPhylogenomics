22c3d88083d99e4047e7803b3d96a3e329342898
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:49:58 +0100

    Create pal2nal.sh

---
+++ selection_analysis/pal2nal.sh
@@ -0,0 +1,9 @@
+#PBS -l walltime=00:20:00
+#PBS -l select=1:ncpus=1:mem=1gb
+
+cd $PBS_O_WORKDIR
+
+/rds/general/user/tbl19/home/software/pal2nal.v14.pal2nal.pl \
+ /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Hsp90_Leca118T_muscle5_msa.fa \
+ /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Hsp90_CDS_Leca118T.fa \
+ -output fasta
