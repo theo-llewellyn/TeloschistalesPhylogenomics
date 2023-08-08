c4a9e4ebceb596497ca5154dd577ad5d033fb024
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:32:56 +0100

    Update iqtree.sh

--- phylogenomics/iqtree.sh
+++ phylogenomics/iqtree.sh
@@ -4,18 +4,14 @@
 module load anaconda3/personal
 source activate iqtree-env 
 
-mkdir IQTree_Leca45T_75p_tAL
-cd IQTree_Leca45T_75p_tAL
+
+mkdir /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca117T_50p_tAL
+cd /rds/general/project/theollewellynproject/live/IQTree/IQTree_Leca117T_50p_tAL
 
 #make concatenated species tree
-iqtree2 -p Leca45T_75p_msa_tAl \
+iqtree2 -p /rds/general/user/tbl19/home/Leca117T_msa_tAl \
  --prefix concat_OF_tAl \
  -B 1000 \
- -T 32
-
-#gene trees
-iqtree2 -S Leca45T_75p_msa_tAl \
- --prefix gene_OF_tAl \
  -T 32
 
 conda deactivate
