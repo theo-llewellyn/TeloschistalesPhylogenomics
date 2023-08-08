c6f60862f1005e11e4a5c493a2dd1adc8711f6cc
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:27:14 +0100

    Update and rename Orthogroups_75percent.R to Orthogroups_50percent.R

--- phylogenomics/Orthogroups_50percent.R
+++ phylogenomics/Orthogroups_50percent.R
@@ -2,6 +2,9 @@ library(tidyr)
 library(dplyr)
 library(readr)
 library(tibble)
+library(janitor)
+
+setwd("~/The Royal Botanic Gardens, Kew/Teloschistales - Documents/0_THEO_LLEWELLYN_PHD/03_RESULTS/ORTHOFINDER/Results_Leca117T/")
 
 #load in data
 Orthogroups <- read_tsv("Orthogroups.GeneCount.tsv")
@@ -10,7 +13,8 @@ head(Orthogroups)
 #filter for rows where all columns are either 0 or 1 and then where the total number of genes is at least half the number of taxa
 #ncol(Orthogroups) - 2 gives the number of taxa as each taxon is a column but there's two extra columns with orthogroup and total which need to be removed
 #can change the 0.75 to higher proportion to reduce amount of missing data
-Orthogroups %>% filter(across(ends_with(c("renamed","proteins")), ~ .x < 2), Total >= (ncol(Orthogroups) -2) * 0.75) -> single_copy_orthologues
+Orthogroups %>% filter(across(ends_with(c("renamed","proteins")), ~ .x < 2), Total >= (ncol(Orthogroups) -2) * 0.5) -> single_copy_orthologues_50
+nrow(single_copy_orthologues_50)
 
 #save orthogroup column as a text file
-write.table(single_copy_orthologues$Orthogroup,"Orthologues_Leca45T_75p.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(single_copy_orthologues_50$Orthogroup,"Orthologues_Leca117T_50p.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
