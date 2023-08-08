bcd080db37bb82bc55bd3085cbbc03e6540395f8
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:51:55 +0100

    Update muscle5.sh

--- selection_analysis/muscle5.sh
+++ selection_analysis/muscle5.sh
@@ -3,7 +3,8 @@
 
 module load anaconda3/personal
 source activate muscle5-env
+
 /rds/general/user/tbl19/home/software/muscle5/muscle5.1.linux_intel64 \
- -align /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Hsp90_Leca118T.fa \
- -output /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Hsp90_Leca118T_muscle5_msa.fa \
+ -align /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Leca117T/Hsp90_Leca117T.fa \
+ -output /rds/general/project/theollewellynproject/live/DNA_Repair_Analysis/Leca117T/Hsp90_Leca117T_muscle5_msa.fa \
  -threads 8
