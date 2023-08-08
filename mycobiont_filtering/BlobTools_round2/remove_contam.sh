f24d8d5b1490ca4e9887da58ad12c6c59b8b6e81
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:19:13 +0100

    Update remove_contam.sh

--- mycobiont_filtering/BlobTools_round2/remove_contam.sh
+++ mycobiont_filtering/BlobTools_round2/remove_contam.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-perl -lane 'if(($F[5]=~"Ascomycota")|($F[5]=~"no-hit")){print $F[0]}' ACCESSION_concoct_blobtools.out/ACCESSION_concoct_blobplot.blobDB.bestsum.table.txt > ACCESSION_concoct_Asco_headers.fa
+filename='macrogen_2022_genomes.txt'
+
+while read p; do 
+    echo $p
+    perl -lane 'if(($F[5]=~"Ascomycota")|($F[5]=~"no-hit")){print $F[0]}' ${p}_concoct_blobtools.out/${p}_concoct_blobplot.blobDB.bestsum.table.txt > /rds/general/project/theollewellynproject/live/filtered_Asco_contigs/${p}_concoct_Asco_headers.txt
+done < $filename
