066b50b0e8393170dfafcf289e3f71a6cbefc0cd
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:35:36 +0100

    Delete anthraquinones directory

--- anthraquinones/hmmalign/hmm_convert.sh
+++
@@ -1,8 +0,0 @@
-#PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=32:mem=96gb
-
-module load hmmer
-
-cd $PBS_O_WORKDIR
-
-hmmbuild Pfam_output.out Pfam_seed.txt
--- anthraquinones/hmmalign/pull_anthraquinone_pks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bah
-
-cat file_noasterisk.fa | seqkit grep -f OG_anthraquinone_headers.txt > OG_anthraquinones.fa
