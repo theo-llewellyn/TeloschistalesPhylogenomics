61f4cb784407ea64814217bdccf6b920f7b22ffe
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 15:33:57 +0100

    Update rename_alignments.sh

--- MCMCTree_84T_genome/rename_alignments.sh
+++ MCMCTree_84T_genome/rename_alignments.sh
@@ -8,3 +8,6 @@ cd ../OG0002170
 sed -i 's/\*\.aln/partitions12_OG0002170_Telos.aln/' */*ctl
 cd ../OG0002971
 sed -i 's/\*\.aln/partitions12_OG0002971_Telos.aln/' */*ctl
+
+sed -i -e 's/_prot[0-9]*[^:]//g' */partitions12_*_Telos.aln
+sed -i -e 's/_[0-9].*-T1//' */partitions12_*_Telos.aln
