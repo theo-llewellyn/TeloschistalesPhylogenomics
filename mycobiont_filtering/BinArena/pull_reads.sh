bb6febf70f745ee30d264f87919d319870665f29
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 11:20:35 +0100

    Update and rename seqkit_array.sh to pull_reads.sh

---
+++ mycobiont_filtering/BinArena/pull_reads.sh
@@ -0,0 +1,27 @@
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=16:mem=124gb
+#PBS -J 1-83
+
+#get name of contigs file
+ACCESSION=$(head -n $PBS_ARRAY_INDEX /rds/general/user/tbl19/home/macrogen_2022_genomes.txt|tail -n 1)
+
+module load anaconda3/personal
+source activate seqkit-env
+
+cd $PBS_O_WORKDIR
+
+#opens the assembly, searches for particular contigs and puts into a new file
+cat /rds/general/project/theollewellynproject/live/filtered_Asco_contigs/${ACCESSION}_concoct_merged_contigs.fa | /rds/general/user/tbl19/home/anaconda3/envs/seqkit-env/bin/seqkit grep -f /rds/general/project/theollewellynproject/live/filtered_Asco_contigs/${ACCESSION}_concoct_Asco_headers.txt > /rds/general/project/theollewellynproject/live/filtered_Asco_contigs/${ACCESSION}_concoct_Asco_contigs.fa
+
+conda deactivate
+
+#pull reads mapping to those contigs with bbmap
+module load bbmap
+
+bbmap.sh -Xmx62g \
+nodisk=t \
+ref=/rds/general/project/theollewellynproject/live/filtered_Asco_contigs/${ACCESSION}_concoct_Asco_contigs.fa \
+in1=/rds/general/project/theollewellynproject/live/data/Trimmed_reads/${ACCESSION}_Trimmed_1P.fq.gz \
+in2=/rds/general/project/theollewellynproject/live/data/Trimmed_reads/${ACCESSION}_Trimmed_2P.fq.gz \
+threads=16 \
+outm=/rds/general/project/theollewellynproject/ephemeral/filtered_Asco_reads/${ACCESSION}_concoct_Asco_R#.fq.gz
