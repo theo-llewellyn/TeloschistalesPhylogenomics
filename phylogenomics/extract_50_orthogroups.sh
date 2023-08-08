9930de83bb7b8036e2720d16aab92799af6da6f3
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 13:27:11 +0100

    Delete phylogenomics directory

--- phylogenomics/extract_50_orthogroups.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir Single_Copy_Orthologues_50percent
-
-cat Orthologues_Leca117T_50p.txt | while read line; do cp Orthogroup_Sequences/${line}.fa Single_Copy_Orthologues_50percent; done
-
