c5e8e042a5bdf75972da515f5686f96cf4bb96bf
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 17:37:51 +0100

    Update prepare_tracer_files.sh

--- MCMCTree_552T_marker/02_MCMCTree/prepare_tracer_files.sh
+++ MCMCTree_552T_marker/02_MCMCTree/prepare_tracer_files.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-seq -f '%.7g' 0 500 3459500 > gens.txt
+cd mcmc_files
+lines=$(wc -l mcmc_tracer.txt | cut -f 1 -d ' ')
+gens=$((($lines - 1)*500))
+
+seq -f '%.7g' 0 500 $gens > gens.txt
 paste -d' ' gens.txt mcmc_tracer.txt > mcmc_tracer_gens.txt
-gcut --complement -f 2 mcmc_tracer_gens.txt > mcmc_tracer_gens.txt1
-mv mcmc_tracer_gens.txt1 > mcmc_tracer_gens.txt
+cut --complement -f 2 mcmc_tracer_gens.txt > mcmc_tracer_gens.txt1
+mv mcmc_tracer_gens.txt1 mcmc_tracer_gens.txt
+
+lines=$(wc -l mcmc_tracer_ILN.txt | cut -f 1 -d ' ')
+gens=$((($lines - 1)*500))
+
+seq -f '%.7g' 0 500 $gens > gens.txt
+paste -d' ' gens.txt mcmc_ILN_tracer.txt > mcmc_ILN_tracer_gens.txt
+cut --complement -f 2 mcmc_ILN_tracer_gens.txt > mcmc_tracer_gens.txt1
+mv mcmc_tracer_gens.txt1 mcmc_ILN_tracer_gens.txt
+
+rm gens.txt
