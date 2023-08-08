901f44f2a91682866b58c4f4b52d5d405fae878a
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2023-08-08 18:17:38 +0100

    Update Combine_MCMC.sh

--- MCMCTree_84T_genome/03_MCMCTree/Combine_MCMC.sh
+++ MCMCTree_84T_genome/03_MCMCTree/Combine_MCMC.sh
@@ -3,22 +3,17 @@
 
 curr_dir=$( pwd )
 mkdir -p mcmc_files
-
 for i in `seq 1 5`
-do
-
-	cd mcmc$i
-	printf "Parsing mcmc"$i"/mcmc.txt ... ... \n"
-	end=$( wc -l mcmc.txt | sed 's/ ..*//' )
-	if [[ $i -eq 1 ]]
-		then
-			begin=1
-		else 
-			begin=2 
-		fi
-	sed -n ''${begin}','${end}'p' mcmc.txt >> $curr_dir/mcmc_files/mcmc_tracer.txt
-	sed -n '1,'${end}'p' mcmc.txt >>  mcmc_clean.txt
-	cd $curr_dir
+ do cd mcmc$i
+ printf "Parsing mcmc"$i"/mcmc.txt ... ... \n"
+ end=$( wc -l mcmc.txt | sed 's/ ..*//' )
+ if [[ $i -eq 1 ]]
+  then begin=1
+  else begin=2
+ fi
+ sed -n ''${begin}','${end}'p' mcmc.txt >>$curr_dir/mcmc_files/mcmc_tracer.txt
+ sed -n '1,'${end}'p' mcmc.txt >>  mcmc_clean.txt
+ cd $curr_dir
 done
 
 
